a05850c81014fc76d194f14c936d2f31da6e9217
Piotr333 <pi-st@o2.pl>
2020-06-20 23:15:47 +0200

    5 wersja

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("4 wersja");
+		System.out.println("5 wersja programu");
 
 	}
 
